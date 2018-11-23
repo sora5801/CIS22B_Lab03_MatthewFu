6e1a6563a9aef03411e104ea75b2df7d5aedc951
Matthew Fu <sora5801fu@aol.com>
2018-11-22 21:07:58 -0800

    This is a new lab

--- CIS22B_Lab03_MatthewFu.cpp
+++ CIS22B_Lab03_MatthewFu.cpp
@@ -17,13 +17,42 @@ public:
 }
 ;
 class ProductionWorker
-{};
+{
+private:
+   int shift;
+   double hourlypayrate;
+public:
+   ProductionWorker();
+   int setshift();
+   double sethourlypayrate();
+
+};
 
 class ShiftSurpervisor: public Employee
-{};
+{
+private:
+   double annualsalary;
+   double annualproductionbonus;
+public:
+
+};
 
 class TeamLeader
-{};
+{
+private:
+   int monthlyBonus;
+   int requiredHours;
+   int trainingHours;
+public:
+   TeamLeader();
+   int getmonthlyBonus();
+   int getrequiredHours();
+   int gettrainingHours();
+   int setmonthlyBonus();
+   int setrequiredHours();
+   int settrainingHours();
+
+};
 
 int main()
 {
